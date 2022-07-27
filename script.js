64d03f937497ecfcd61668c175356610e8d3d976
dunmeri <ethanq95@gmail.com>
2022-07-27 22:24:20 +0700

    Add new features
    -Improve calculator appearance;
    -Add 'backspace' button.

--- script.js
+++ script.js
@@ -48,24 +48,35 @@ function getResult() {
 
 function setResult(result) {
     let resultString = document.querySelector('#result');
-    resultString.textContent = result;
+    if (result.length > 21) {
+        resultString.textContent = 'Overflow';
+    }
+    else {
+        resultString.textContent = result;
+    }
 }
 
 
 function pressButton(event) {
     if (event.target.className === 'digit') {
         if (newLine === false && pointIsPressed === false) {
-            currentNumber = Number(`${currentNumber}${event.target.id}`);
-            setResult(currentNumber);
+            if (currentNumber !== '0') {
+                currentNumber = `${currentNumber}${event.target.id}`;
+                setResult(currentNumber);
+            }
+            else {
+                currentNumber = `${event.target.id}`;
+                setResult(currentNumber);
+            }
         }
         else if (newLine === false && pointIsPressed === true) {
-            currentNumber = Number(`${currentNumber}.${event.target.id}`);
+            currentNumber = `${currentNumber}.${event.target.id}`;
             pointIsPressed = false;
             setResult(currentNumber);
         }
         else {
             lastNumber = currentNumber;
-            currentNumber = Number(`${event.target.id}`);
+            currentNumber = `${event.target.id}`;
             newLine = false;
             setResult(currentNumber);
         }
@@ -73,17 +84,17 @@ function pressButton(event) {
 
     else if (event.target.className == 'operator') {
         if (operatorIsPressed === false) {
-            lastNumber = currentNumber;
+            lastNumber = Number(currentNumber);
             operator = event.target.textContent;
-            currentNumber = 0;
+            currentNumber = '0';
             newLine = false;
             operatorIsPressed = true;
             setResult(currentNumber);
         }
         else {
             calculate();
-            lastNumber = currentNumber;
-            currentNumber = 0;
+            lastNumber = Number(currentNumber);
+            currentNumber = '0';
             operator = event.target.textContent;
             newLine = false;
             operatorIsPressed = true;
@@ -93,9 +104,9 @@ function pressButton(event) {
 
 
 function calculate() {    
-    let result = operate(lastNumber, currentNumber, operator);
-    currentNumber = result;
-    setResult(result);
+    let result = operate(lastNumber, Number(currentNumber), operator);
+    currentNumber = (Math.round(result * 10000) / 10000).toString();
+    setResult(currentNumber);
     operator = null;
     newLine = true;
     operatorIsPressed = false;
@@ -105,14 +116,27 @@ function calculate() {
 function deleteAll() {
     operatorIsPressed = false;
     lastNumber = 0;
-    currentNumber = 0;
+    currentNumber = '0';
     newLine = false;
     setResult(currentNumber);
 }
 
 
+function deleteLastChar() {
+    console.log('here')
+    if (currentNumber.length > 1) {
+        currentNumber = currentNumber.slice(0, -1);
+        setResult(currentNumber);
+    }
+    else {
+        currentNumber = '0';
+        setResult(currentNumber);
+    }
+}
+
+
 function switchPoint() {
-    if (currentNumber % 1 === 0) {
+    if (currentNumber.indexOf('.') === -1) {
         pointIsPressed = true;
     }
 }
@@ -120,7 +144,7 @@ function switchPoint() {
 
 let operator = null;
 let lastNumber = 0;
-let currentNumber = 0;
+let currentNumber = '0';
 let newLine = false;
 let operatorIsPressed = false;
 let pointIsPressed = false;
@@ -136,3 +160,6 @@ pointButton.addEventListener('click', switchPoint);
 
 const deleteButton = document.querySelector('#delete');
 deleteButton.addEventListener('click', deleteAll);
+
+const backspaceButton = document.querySelector('#backspace');
+backspaceButton.addEventListener('click', deleteLastChar);
